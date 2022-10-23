fb3e4062a1aceba49917738d23bdd3896c7f4a33
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-10-23 12:11:25 +0700

    Update screen bpjs kesehatan

--- lib/screens/features/bpjs/bpjs_kesehatan/bpjs_kesehatan.dart
+++ lib/screens/features/bpjs/bpjs_kesehatan/bpjs_kesehatan.dart
@@ -1,3 +1,4 @@
+import 'package:bmt_kbs/etc/color_pallete.dart';
 import 'package:bmt_kbs/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,7 @@ class BpjsKesehatanScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(60),
@@ -16,36 +18,124 @@ class BpjsKesehatanScreen extends StatelessWidget {
         ),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 30.0,
-              vertical: 20.0,
+            padding: const EdgeInsets.only(
+              left: 30.0,
+              right: 30.0,
+              top: 10.0,
+              bottom: 20.0,
             ),
-            child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        child: Image.asset(
+                          "assets/screens/bpjs/logo_bpjs2.png",
+                          scale: 1.6,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      const Text("BPJS Kesehatan"),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Wrap(
+                  runSpacing: 10,
+                  children: [
+                    const Text(
+                      "ID Pelanggan",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        fillColor: ColorPallete.lightBlueColor,
+                        hintText: "Masukkan No. BPJS",
+                        hintStyle: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 28,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Bayar Hingga",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        minimumSize: Size.zero, // Set this
+                        padding: EdgeInsets.zero, // and this
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Pilih Bulan",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+            child: SizedBox(
+              height: 60,
               width: double.infinity,
-              height: 50,
-              decoration: const BoxDecoration(
-                color: Colors.amber,
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    width: 50,
-                    height: double.infinity,
-                    decoration: const BoxDecoration(
-                      color: Colors.red,
-                    ),
-                    child: Image.asset(
-                      "assets/screens/bpjs/logo_bpjs2.png",
-                    ),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: ColorPallete.primaryColor,
+                ),
+                child: const Text(
+                  "Cek Tagihan",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
                   ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  const Text("BPJS Kesehatan"),
-                ],
+                ),
               ),
             ),
           ),
